9e20cf7cbcbd9cf0125135e7d25b30f14850f830
Lucas Souza <outrolucas2002@gmail.com>
2022-12-20 17:46:01 -0300

    feat: create ProductsTable component

---
+++ src/components/ProductsTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Active</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Product</td>
+          <td>true</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ClientsTable",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
   <main>
-    <clients-table />
+    <div class="flex flex-row">
+      <clients-table class="w-2/3" />
+      <products-table class="w-1/3" />
+    </div>
   </main>
 </template>
 
 <script setup>
 import ClientsTable from ".././components/ClientsTable.vue";
+import ProductsTable from ".././components/ProductsTable.vue";
 </script>
